28193d4f8b51164f871a75966aedadb8070bd6b1
Gianluca Mariani <gi.mariani@reply.it>
2026-01-29 11:56:52 +0100

    fixed scrollbar

--- resources/views/livewire/chat-bot.blade.php
+++ resources/views/livewire/chat-bot.blade.php
@@ -31,7 +31,7 @@
 
         <div id="messages"
             wire:key="chatgpt-agent-messages"
-            style="overflow: auto; min-height: max(20rem, 30vh); max-height: calc(100vh - 11rem); padding-bottom: 1rem; margin-bottom: 65px;"
+            style="overflow: auto; min-height: max(20rem, 30vh); max-height: calc(100vh - 18rem); padding-bottom: 1rem; margin-bottom: 65px;"
             class="flex flex-col space-y-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
             @foreach ($messages as $message)
                 @if ($message['role'] !== 'system')
